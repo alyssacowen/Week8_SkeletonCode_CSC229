6f056d695e3b24d95cb91d841c6f990f2471a884
alyssacowen <alyssarcowen@gmail.com>
2023-03-27 12:34:05 -0400

    Linear Search For Loop Ran and Tested Linear Search Called Binary Search Method in Main to test the algorithm Completed Bubble Sort Method then tested the algorithm

--- src/main/java/com/mycompany/week8_skeletoncode_lab/Main.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/Main.java
@@ -5,6 +5,7 @@
 package com.mycompany.week8_skeletoncode_lab;
 
 import static com.mycompany.week8_skeletoncode_lab.BinarySearch.runBinarySearchIteratively;
+import static com.mycompany.week8_skeletoncode_lab.BubbleSort.bubbleSort;
 import static com.mycompany.week8_skeletoncode_lab.LinearSearch.search;
 
 
@@ -20,19 +21,28 @@ public class Main {
         int x = 7;
         int Linearindex = search(arr,x);
         if (Linearindex > -1){
-            System.out.println("During the linear search, integer " + x + " is in the array at index " + Linearindex);
+            System.out.println("During the linear search, integer " + x + " is in the arr array at index " + Linearindex);
         }
         else{
-            System.out.println("There is no occurence of integer " + x + " in the array");
+            System.out.println("There is no occurence of integer " + x + " in the arr array");
              //I chose 7 as the x and since 7 is in the array, after running this code, the output was "During the linear search, integer 7 is in the array at index 2"
         }
         //Binary Search
-         int[] sortedArray = {4,5,7,9,20,31};
-        int key = 20;
+        int[] sortedArray = {1,19,31,44,77,84};
+        int key = 19;
         int low = 0;
         int high = 5;
         int index = runBinarySearchIteratively(sortedArray, key, low, high);
-        System.out.println("During the binary search, integer " + key + " can be found at index " + index);
+        System.out.println("During the binary search, integer " + key + " can be found at index " + index + " in the array sortedArray.");
+        
+        //Bubble Sort
+        int[] a = {55,920,705,2,72,19};
+        int size=6;
+        bubbleSort(a, size);
+        System.out.println("Array 'a' sorted after using bubble sort method: ");
+        for (int i = 0; i < size; i++) {
+            System.out.println(a[i] + " ");
+        }
     }
     }
 
