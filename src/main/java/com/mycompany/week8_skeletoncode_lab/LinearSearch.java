af22dea6f3120c4594e1cd3b5c3cf52a40936b52
alyssacowen <alyssarcowen@gmail.com>
2023-03-26 20:03:34 -0400

    Linear Search For Loop Ran and Tested Linear Search

--- src/main/java/com/mycompany/week8_skeletoncode_lab/LinearSearch.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/LinearSearch.java
@@ -13,13 +13,29 @@ public class LinearSearch {
     public static int search(int arr[], int x)
     {
         int n = arr.length;
+        
         // Todo 01: - complete the implementation of linear search and test your code
         for (int i = 0; i < n ; i++) {
             if(arr[i] == x){
                 return i; //x is the integer/element that is being searched for, this will iterate through the array's elements, if x is found it will return i which is the index x can be found at in the array
             }
         }
-         //         - prvoide asymptotic analysis of the developed solution
+        return -1; //during the linear search, if x is never found, this function will return -1
+//         - provide asymptotic analysis of the developed solution
+//
     }
+    public static void main(String[] args) {
+        int arr[]={5,20,7,9,31,4};
+        int x = 7;
+        int index = search(arr,x);
+        if (index > -1){
+            System.out.println("Integer " + x + " is in the array at index " + index);
+        }
+        else{
+            System.out.println("There is no occurence of integer " + x + " in the array");
+        }
+    }
+    
+    
     
 }
