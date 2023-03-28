8789e74d33a2a814dda17cc9ec8c82eaa26083d7
alyssacowen <alyssarcowen@gmail.com>
2023-03-27 20:15:31 -0400

    Linear Search For Loop Implementation Ran and Tested Linear Search Called Binary Search Method in Main to test the algorithm Completed Bubble Sort Method then tested the algorithm in Main Prime checker and Sum of Prime Code

--- src/main/java/com/mycompany/week8_skeletoncode_lab/Problem01.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/Problem01.java
@@ -10,11 +10,35 @@ package com.mycompany.week8_skeletoncode_lab;
  */
 public class Problem01 {
     
+    public static boolean isPrime(int n){
+        if (n<=1){
+            return false;
+        }
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
     public static long getSumOfPrimes(int n){
+        long sum = 0;
+        long result;
+    if (n == 1)
+    result = 1;
+        else
+            result = n + getSumOfPrimes(n-1);
+                return result;
+}
+
+}
+  
+
+
+
     // Todo 04: Develop a method that returns the sum of the prime numbers between 1 and n
     //          Test your solution
     //          Analyze its space and time  
     
-    }
     
-}
+  
